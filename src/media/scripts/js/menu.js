153f642e879c075c931306d98445eefa7066577c
BluNay <79167652+BluNay@users.noreply.github.com>
2024-11-24 22:38:51 +0400

    Ajout du responsive
    Responsive pour les écrans (@media max-425px) et (@media max-768px)

--- src/media/scripts/js/menu.js
+++ src/media/scripts/js/menu.js
@@ -1,18 +1,9 @@
-const burger = document.querySelector(".burger");
-const headerMenu = document.querySelector(".header .menu");
-const headerMenuItems = document.querySelectorAll(".header .menu .item");
+document.getElementById("openBtn").addEventListener("click", function (e) {
+	e.preventDefault();
+	document.getElementById("mySidenav").classList.add("active");
+});
 
-burger.addEventListener("click", () => {
-	headerMenu.classList.toggle("open");
-	burger.classList.toggle("open");
-
-	headerMenuItems.forEach((item, index) => {
-		if (item.style.animationDelay) return (item.style.animationDelay = "");
-
-		const maxDelay = 0.8;
-		const delay = index / 12;
-		const animationDelay = delay >= maxDelay ? maxDelay : delay;
-
-		return (item.style.animationDelay = `${animationDelay}s`);
-	});
+document.getElementById("closeBtn").addEventListener("click", function (e) {
+	e.preventDefault();
+	document.getElementById("mySidenav").classList.remove("active");
 });
